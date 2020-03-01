43c1263b5e33ab22f65ac6360a1c9bee2af89662
rafi993 <rafi993@protonmail.com>
2020-03-01 16:45:39 +0530

    Adding useContextMenu hook

---
+++ src/hooks/__test__/useContextMenu.test.js
@@ -0,0 +1,7 @@
+import useContextMenu from '../useContextMenu';
+
+describe('Testing useContextMenu hook', () => {
+  it.todo('displays context menu');
+  it.todo('hides context menu on left click');
+  it.todo('hides context menu on click outside');
+});
